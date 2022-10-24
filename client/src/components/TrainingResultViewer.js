b3a0f5ee87900bcb39e6137f86405b17ab4eeb0c
darwin1501 <darwin.pablo1501@gmail.com>
2022-10-24 15:07:09 +0800

    show score in percentage

--- client/src/components/TrainingResultViewer.js
+++ client/src/components/TrainingResultViewer.js
@@ -26,14 +26,19 @@ const TrainingResultViewer = (props) => {
         totalQuestionCount += count[1]
     });
 
+    function getScoreInPercentage(value, totalValue) {
+        return (value / totalValue ) * 100
+    }
+
     return (
-        <div className="flex flex-column">
-            <p>Your Score: {mainScore} / {totalQuestionCount}</p>
-            {numericalCount > 0 && <p>Numerical: {numerical} / { numericalCount }</p>}
-            {analyticalCount > 0&& <p>Analytical: {analytical} / { analyticalCount }</p>}
-            {verbalCount > 0 && <p>Verbal: {verbal} / { verbalCount }</p>}
-            {philConsCount > 0 && <p>Philippine Constitution: {philConstitution} / { philConsCount }</p>}
-            {clericalCount > 0 && <p>Clerical: {clerical} / { clericalCount }</p>}
+        <div className="flex flex-column align-center">
+            <h2>{mainScore} / {totalQuestionCount} ({getScoreInPercentage(mainScore, totalQuestionCount)}%)</h2>
+            <h4 style={{margin: "0"}}>Your Score</h4>
+            {numericalCount > 0 && <p>Numerical: {numerical} / {numericalCount} ({ getScoreInPercentage(numerical, numericalCount)}%)</p>}
+            {analyticalCount > 0&& <p>Analytical: {analytical} / { analyticalCount } ({ getScoreInPercentage(analytical, analyticalCount)}%)</p>}
+            {verbalCount > 0 && <p>Verbal: {verbal} / { verbalCount } ({ getScoreInPercentage(verbal, verbalCount)}%)</p>}
+            {philConsCount > 0 && <p>Philippine Constitution: {philConstitution} / { philConsCount } ({ getScoreInPercentage(philConstitution, philConsCount)}%)</p>}
+            {clericalCount > 0 && <p>Clerical: {clerical} / { clericalCount } ({ getScoreInPercentage(clerical, clericalCount)}%)</p>}
         </div>
     )
 }
