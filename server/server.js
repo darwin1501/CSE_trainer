de51bc9389bdbb7b64aabfa947d713b21aa3d0a1
darwin1501 <darwin.pablo1501@gmail.com>
2022-11-23 16:06:44 +0800

    add url to cors

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(require("./routes/question"));
 // get driver connection
